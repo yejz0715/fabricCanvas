9cfb0e2d455ccf513f66a223488df20c416d0345
yejz0715 <yejz0715@gmail.com>
2023-12-14 23:36:39 +0900

    프로젝트 초기설정

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+/**
+ * 애플리케이션 진입점
+ * @returns{JSX.Element} App 컴포넌트
+ */
+const App = (): JSX.Element => {
+    return <div className="text-3xl font-bold underline">hi</div>;
+};
+
+export default App;
