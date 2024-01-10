8517a7104c03d16cd058405df5fe56359925025d
yejz0715 <yejz0715@gmail.com>
2024-01-10 23:27:34 +0900

    Add : dummy data 추가

---
+++ src/dummy.ts
@@ -0,0 +1,20 @@
+/** stickerList 타입 */
+type StickerListType = {
+  name: string;
+  url: string;
+};
+/** 캔버스 도구_스티커 목록 */
+export const stickerList: StickerListType[] = [
+  {
+    name: "house",
+    url: "https://cdn-icons-png.flaticon.com/128/2544/2544087.png?ga=GA1.1.55221550.1704800589&semt=sph",
+  },
+  {
+    name: "dog",
+    url: "https://cdn-icons-png.flaticon.com/128/1998/1998627.png?ga=GA1.1.55221550.1704800589&semt=sph",
+  },
+  {
+    name: "apple",
+    url: "https://cdn-icons-png.flaticon.com/128/415/415682.png?ga=GA1.1.55221550.1704800589&semt=sph",
+  },
+];
